5ad6781c42b13d97c8117edc3cce97c50aa66faa
Edwin J Fernandez <fernandez.edwin86@protonmail.com>
2023-02-28 19:03:31 -0500

    Finish Static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'static_pages#home'
+
+  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial DVS Sample App V2"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
